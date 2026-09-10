7814ff37b3914d35dbb4acdac701e6f923db3f2b
Ramla-ALI <164019229+Ramla-ALI@users.noreply.github.com>
2026-09-10 09:58:25 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        int timeOfDay = 2;
+
+        String result = getGreeting(timeOfDay);
+        System.out.println(result);
+    }
+
+    public static String getGreeting(int timeOfDay) {
+        if(timeOfDay >= 5 && timeOfDay < 12){
+            return "Good morning!";
+        } else if(timeOfDay >= 12 && timeOfDay <= 18){
+            return "Good afternoon!";
+        } else {
+            return "Good evening!";
+        }
+    }
+}
